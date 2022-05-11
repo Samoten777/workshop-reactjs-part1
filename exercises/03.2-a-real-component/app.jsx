ecf0ce449a18fce25083f5a2b64d4e39ee2af725
Laure GAGNER <laure.gagner@epitech.eu>
2022-05-11 10:55:35 -0400

    [add] exo 03.4

--- exercises/03.2-a-real-component/app.jsx
+++ exercises/03.2-a-real-component/app.jsx
@@ -3,5 +3,55 @@ import ReactDOM from "react-dom";
 
 //create your function here
 
+const BootstrapCard = () => (
+    <div className="card m-5">
+        <img className="card-img-top" src="../../.learn/assets/Dylan.png?raw=true" alt="Card image cap" />
+        <div className="card-body">
+            <h5 className="card-title">Bob Dylan</h5>
+            <p className="card-text">Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer/songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.</p>
+            <a href="https://en.wikipedia.org/wiki/Bob_Dylan" className="btn btn-primary">Go to wikipedia</a>
+        </div>
+    </div>
+)
+
 //remember to use ReactDOM.render to include the component into the website
 
+ReactDOM.render(<BootstrapCard/>, document.querySelector("#myDiv"));
+
+import React from "react";
+import ReactDOM from "react-dom";
+import PropType from "prop-types";
+
+const BootstrapCard = (props) => {
+	// 1) replace the hard-coded image, description, link, etc. With their property variable.
+	return (
+		<div className="card m-5">
+			<img className="card-img-top" src={props.imageURL} alt="Card image cap" />
+			<div className="card-body">
+				<h5 className="card-title">{props.title}</h5>
+				<p className="card-text">{props.description}</p>
+				<a href={props.buttonUrl} className="btn btn-primary">
+					{props.buttonLabel}
+				</a>
+			</div>
+		</div>
+	);
+};
+BootstrapCard.propTypes = {
+	title: PropType.string,
+    imageUrl: PropType.string,
+    description: PropType.string,
+    buttonUrl: PropType.string,
+    buttonLabel: PropType.string
+		// 2) add here the new properties into the proptypes object
+};
+
+
+// 3) Use ReactDOM to add the component into then DOM element #myDiv
+ReactDOM.render(<BootstrapCard
+title="Bob Dylan"
+imageURL=""
+description=""
+buttonUrl=""
+buttonLabel="Go to Wikipedia"
+/>, document.querySelector("#myDiv"));
